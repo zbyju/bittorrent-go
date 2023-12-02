03159775b4025cbf4cc09d33b84ee2ad3e4b716c
zbyju <zbynek.jurica@gmail.com>
2023-12-02 21:12:21 +0100

    Add parsing of a torrent file

---
+++ cmd/mybittorrent/decode/bencode.go
@@ -0,0 +1,144 @@
+package decode
+
+import (
+	"errors"
+	"strconv"
+	"unicode"
+)
+
+// Example:
+// - 5:hello -> hello
+// - 10:hello12345 -> hello12345
+func DecodeBencode(bencodedString string) (interface{}, error) {
+	res, err := Decode(bencodedString, 0)
+
+	if err != nil {
+		return "", err
+	}
+
+	return res.result, nil
+}
+
+type decodeResult struct {
+	result interface{}
+	index  int
+}
+
+func Decode(bencodedString string, index int) (decodeResult, error) {
+	var res decodeResult
+	var err error
+
+	if unicode.IsDigit(rune(bencodedString[index])) {
+		res, err = DecodeString(bencodedString, index)
+	} else if bencodedString[index] == 'i' {
+		res, err = DecodeInt(bencodedString, index)
+	} else if bencodedString[index] == 'l' {
+		res, err = DecodeList(bencodedString, index)
+	} else if bencodedString[index] == 'd' {
+		res, err = DecodeDict(bencodedString, index)
+	} else {
+		return decodeResult{}, errors.New("Input string needs to be in format of String, Int, Array, Dict. Found: " + string(bencodedString[index]))
+	}
+
+	if err != nil {
+		return decodeResult{}, nil
+	}
+
+	return res, nil
+}
+
+func decodeRawString(bencodedString string, index int) (string, int, error) {
+	var firstColonIndex int
+
+	for i := index; i < len(bencodedString); i++ {
+		if bencodedString[i] == ':' {
+			firstColonIndex = i
+			break
+		}
+	}
+
+	lengthStr := bencodedString[index:firstColonIndex]
+
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", 0, err
+	}
+
+	res := bencodedString[firstColonIndex+1 : firstColonIndex+1+length]
+
+	return res, firstColonIndex + length + 1, nil
+}
+
+func DecodeString(bencodedString string, index int) (decodeResult, error) {
+	res, index, err := decodeRawString(bencodedString, index)
+
+	if err != nil {
+		return decodeResult{}, err
+	}
+
+	return decodeResult{res, index}, nil
+}
+
+func DecodeInt(bencodedString string, index int) (decodeResult, error) {
+	var firstEIndex int
+
+	for i := index; i < len(bencodedString); i++ {
+		if bencodedString[i] == 'e' {
+			firstEIndex = i
+			break
+		}
+	}
+
+	numStr := bencodedString[index+1 : firstEIndex]
+
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		return decodeResult{}, err
+	}
+
+	return decodeResult{num, firstEIndex + 1}, nil
+}
+
+func DecodeList(bencodedString string, index int) (decodeResult, error) {
+	list := []interface{}{}
+	index += 1
+
+	for bencodedString[index] != 'e' {
+		res, err := Decode(bencodedString, index)
+
+		if err != nil {
+			return decodeResult{}, err
+		}
+
+		list = append(list, res.result)
+		index = res.index
+	}
+
+	return decodeResult{list, index + 1}, nil
+}
+
+func DecodeDict(bencodedString string, index int) (decodeResult, error) {
+	dict := make(map[string]interface{})
+	index += 1
+
+	for bencodedString[index] != 'e' {
+		key, i, err := decodeRawString(bencodedString, index)
+
+		if err != nil {
+			return decodeResult{}, err
+		}
+
+		index = i
+
+		val, err := Decode(bencodedString, index)
+
+		if err != nil {
+			return decodeResult{}, err
+		}
+
+		dict[key] = val.result
+		index = val.index
+	}
+
+	return decodeResult{dict, index + 1}, nil
+}
--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,149 +2,21 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
-	"strconv"
-	"unicode"
+
+	"github.com/codecrafters-io/bittorrent-starter-go/cmd/mybittorrent/decode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	res, err := decode(bencodedString, 0)
-
-	if err != nil {
-		return "", err
+type TorrentFile struct {
+	Announce string `json:"announce"`
+	Info     struct {
+		Length      int    `json:"length"`
+		Name        string `json:"name"`
+		PieceLength int    `json:"piece length"`
+		Pieces      string `json:"pieces"`
 	}
-
-	return res.result, nil
-}
-
-type decodeResult struct {
-	result interface{}
-	index  int
-}
-
-func decode(bencodedString string, index int) (decodeResult, error) {
-	var res decodeResult
-	var err error
-
-	if unicode.IsDigit(rune(bencodedString[index])) {
-		res, err = decodeString(bencodedString, index)
-	} else if bencodedString[index] == 'i' {
-		res, err = decodeInt(bencodedString, index)
-	} else if bencodedString[index] == 'l' {
-		res, err = decodeList(bencodedString, index)
-	} else if bencodedString[index] == 'd' {
-		res, err = decodeDict(bencodedString, index)
-	} else {
-		return decodeResult{}, errors.New("Input string needs to be in format of String, Int, Array, Dict. Found: " + string(bencodedString[index]))
-	}
-
-	if err != nil {
-		return decodeResult{}, nil
-	}
-
-	return res, nil
-}
-
-func decodeRawString(bencodedString string, index int) (string, int, error) {
-	var firstColonIndex int
-
-	for i := index; i < len(bencodedString); i++ {
-		if bencodedString[i] == ':' {
-			firstColonIndex = i
-			break
-		}
-	}
-
-	lengthStr := bencodedString[index:firstColonIndex]
-
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil {
-		return "", 0, err
-	}
-
-	res := bencodedString[firstColonIndex+1 : firstColonIndex+1+length]
-
-	return res, firstColonIndex + length + 1, nil
-}
-
-func decodeString(bencodedString string, index int) (decodeResult, error) {
-	res, index, err := decodeRawString(bencodedString, index)
-
-	if err != nil {
-		return decodeResult{}, err
-	}
-
-	return decodeResult{res, index}, nil
-}
-
-func decodeInt(bencodedString string, index int) (decodeResult, error) {
-	var firstEIndex int
-
-	for i := index; i < len(bencodedString); i++ {
-		if bencodedString[i] == 'e' {
-			firstEIndex = i
-			break
-		}
-	}
-
-	numStr := bencodedString[index+1 : firstEIndex]
-
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		return decodeResult{}, err
-	}
-
-	return decodeResult{num, firstEIndex + 1}, nil
-}
-
-func decodeList(bencodedString string, index int) (decodeResult, error) {
-	list := []interface{}{}
-	index += 1
-
-	for bencodedString[index] != 'e' {
-		res, err := decode(bencodedString, index)
-
-		if err != nil {
-			return decodeResult{}, err
-		}
-
-		list = append(list, res.result)
-		index = res.index
-	}
-
-	return decodeResult{list, index + 1}, nil
-}
-
-func decodeDict(bencodedString string, index int) (decodeResult, error) {
-	dict := make(map[string]interface{})
-	index += 1
-
-	for bencodedString[index] != 'e' {
-		key, i, err := decodeRawString(bencodedString, index)
-
-		if err != nil {
-			return decodeResult{}, err
-		}
-
-		index = i
-
-		val, err := decode(bencodedString, index)
-
-		if err != nil {
-			return decodeResult{}, err
-		}
-
-		dict[key] = val.result
-		index = val.index
-	}
-
-	return decodeResult{dict, index + 1}, nil
 }
 
 func main() {
@@ -153,7 +25,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := decode.DecodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -161,6 +33,40 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
+	} else if command == "info" {
+		if len(os.Args) != 3 {
+			fmt.Println("Usage: info [path/to/file.torrent]")
+			return
+		}
+
+		file_path := os.Args[2]
+		f, err := os.ReadFile(file_path)
+
+		if err != nil {
+			return
+		}
+
+		decoded, err := decode.DecodeBencode(string(f))
+		if err != nil {
+			fmt.Println("Error decoding bencode:", err)
+			return
+		}
+
+		jsonOutput, err := json.Marshal(decoded)
+		if err != nil {
+			fmt.Println("Error marshalling to JSON:", err)
+			return
+		}
+
+		var output TorrentFile
+		err = json.Unmarshal([]byte(jsonOutput), &output)
+		if err != nil {
+			fmt.Println("Error unmarshalling from JSON:", err)
+			return
+		}
+
+		fmt.Println("Tracker URL:", output.Announce)
+		fmt.Println("Length:", output.Info.Length)
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
